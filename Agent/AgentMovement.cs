33a317c633ff4739c2dfe9c24de0aa0eb5f78d28
BeNeNuTs <david13.tcm@hotmail.fr>
2015-10-21 11:40:43 +0200

    AgentMovements
    - Ajout méthode Wiggle
    - Ajout méthode GoToEnemy
    - Détection d'un ennemi dans le champ de perception

--- Agent/AgentMovement.cs
+++ Agent/AgentMovement.cs
@@ -3,13 +3,55 @@ using System.Collections;
 
 public class AgentMovement : MonoBehaviour {
 
+	public float speed;
+
+	Rigidbody agentRigidbody;
+	NavMeshAgent navMesh;
+
+	Transform target;
+	bool findEnemy = false;
+
 	// Use this for initialization
 	void Start () {
-	
+		navMesh = GetComponent<NavMeshAgent>();
+		agentRigidbody = GetComponent<Rigidbody>();
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.CompareTag("Enemy") && !findEnemy){
+			findEnemy = true;
+			target = other.transform;
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+		if(other.CompareTag("Enemy") && findEnemy){
+			if(other.gameObject == target.gameObject){
+				findEnemy = false;
+				target = null;
+			}
+		}
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
+		if(findEnemy){
+			GoToEnemy ();
+		}else{
+			Wiggle ();
+		}
+	}
+
+	void GoToEnemy(){
+		navMesh.SetDestination(target.position);
+	}
+
+	void Wiggle(){
+		// Faire avancer l'agent
+		agentRigidbody.velocity = transform.forward * speed * Time.fixedDeltaTime;
 	
+		// Rotation de l'agent en Y entre -10 et +10
+		Vector3 rotation = new Vector3(0,Random.Range(-10,10), 0) + agentRigidbody.rotation.eulerAngles;
+		agentRigidbody.rotation = Quaternion.Euler(rotation);
 	}
 }

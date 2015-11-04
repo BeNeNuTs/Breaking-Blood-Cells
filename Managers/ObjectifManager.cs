df25936ca588805b59a4b146a14042900d4f7c19
cedrictardella <cedric.tardella@wanadoo.fr>
2015-11-04 16:46:30 +0100

    Avancée de la partie XML
    Vérification et correction du chargement d'un fichier XML pour les
    objectifs.

--- Managers/ObjectifManager.cs
+++ Managers/ObjectifManager.cs
@@ -12,10 +12,13 @@ public class ObjectifManager : MonoBehaviour {
 	[HideInInspector]
 	public enum tag {KILLBACTERIA = 0, KILLVIRUS = 1, GOTO = 2, ANALYZEBACTERIA = 3, ANALYZEVIRUS = 4, SURVIVE = 5, CREATEANTIBODIES = 6};
 
-	// Use this for initialization
-	void Start () {
-
-	}
+/*	void Start(){
+		//Debug.Log (File.Exists(Application.dataPath+"\\Xml\\xmlTest.xml"));
+		load (Application.dataPath + "\\Xml\\xmlTest.xml", 0);
+		foreach(int i in objectifCourant.tableDesObjectifs.Keys){
+			Debug.Log (i + " -> " + objectifCourant.tableDesObjectifs[i]);
+		}
+	}*/
 
 	// met à jour un objectif en fonction du second paramètre
 	void updateGoal(int label, int type = 0){
@@ -70,26 +73,27 @@ public class ObjectifManager : MonoBehaviour {
 
 		XmlTextReader myXmlTextReader = new XmlTextReader (path);
 		while(myXmlTextReader.Read()){
-			if(myXmlTextReader.IsStartElement()){
+			if(myXmlTextReader.IsStartElement() && myXmlTextReader.Name == "objectif"){
 				if (int.Parse(myXmlTextReader.GetAttribute("id")) == id){
+					aTrouve = true;
 					objectifCourant.description = myXmlTextReader.GetAttribute("description");
 					tag = int.Parse(myXmlTextReader.GetAttribute("tag"));
 					if(tag == 0 || tag == 1 || tag == 3 || tag == 4 || tag == 6){ // Si on a ces tags, on a forcément un int dans value
 						objectifCourant.tableDesObjectifs.Add(tag, (int)int.Parse(myXmlTextReader.GetAttribute("value")));
+						break;
 					} else if(tag == 2){ // Pour le tag 2, on récupère un vector2 dans value sous forme "float;float"
 						coordMatchX = coordRegexX.Match(myXmlTextReader.GetAttribute("value"));
 						coordMatchY = coordRegexY.Match(myXmlTextReader.GetAttribute("value"));
 						coords.Set(float.Parse(coordMatchX.Value), float.Parse(coordMatchY.Value));
 						objectifCourant.tableDesObjectifs.Add(tag, (Vector2)coords);
+						break;
 					} else if(tag == 5){ // Pour le tag 5 on a un float dans value
 						objectifCourant.tableDesObjectifs.Add(tag, (float)float.Parse(myXmlTextReader.GetAttribute("value")));
+						break;
 					}
-
-					aTrouve = true;
 				}
 			}
 		}
-
 		if (!aTrouve)
 			Debug.Log ("N'a pas trouvé.\n");
 	}
---
+++ Objectifs/objectifTest.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class objectifTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine ("processTest");
+	}
+
+	IEnumerator processTest(){
+		yield return new WaitForSeconds(1);
+	}
+}

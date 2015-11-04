81f855db65f876eb9d72d4352f09e2ee0a2226d0
cedrictardella <cedric.tardella@wanadoo.fr>
2015-11-04 11:53:50 +0100

    Avancement des objectifs

--- Managers/ObjectifManager.cs
+++ Managers/ObjectifManager.cs
@@ -9,7 +9,7 @@ public class ObjectifManager : MonoBehaviour {
 	
 	Objectif objectifCourant = new Objectif();
 	[HideInInspector]
-	public enum tag {KILLBACTERIA = 0, KILLVIRUS = 1, GOTO = 2, ANALYZEBACTERIA = 3, ANALYZEVIRUS = 4};
+	public enum tag {KILLBACTERIA = 0, KILLVIRUS = 1, GOTO = 2, ANALYZEBACTERIA = 3, ANALYZEVIRUS = 4, SURVIVE = 5, CREATEANTIBODIES = 6};
 
 	// Use this for initialization
 	void Start () {
@@ -20,17 +20,42 @@ public class ObjectifManager : MonoBehaviour {
 	// Si le label n'est pas présent dans la table, la fonction ne fait rien
 	void updateGoal(int label){
 		if(objectifCourant.tableDesObjectifs.ContainsKey(label)){
+			int tmp = 0;
 			switch(label){
 			case 0:
-				objectifCourant.tableDesObjectifs[label] -= 1;
+				tmp = (int) objectifCourant.tableDesObjectifs[label];
+				--tmp;
+				if(tmp != 0)
+					objectifCourant.tableDesObjectifs[label] = tmp;
+				else
+					objectifCourant.tableDesObjectifs.Remove(label);
+
+				//TODO : isComplete
 				break;
 			case 1:
+				tmp = (int) objectifCourant.tableDesObjectifs[label];
+				--tmp;
+				if(tmp != 0)
+					objectifCourant.tableDesObjectifs[label] = tmp;
+				else
+					objectifCourant.tableDesObjectifs.Remove(label);
+
+				//TODO : isComplete
 				break;
 			case 2:
+				//TODO : isComplete
 				break;
 			case 3:
+				//TODO : isComplete
 				break;
 			case 4:
+				//TODO : isComplete
+				break;
+			case 5:
+				//TODO : isComplete
+				break;
+			case 6:
+				//TODO : isComplete
 				break;
 			default:
 				break;
@@ -40,6 +65,6 @@ public class ObjectifManager : MonoBehaviour {
 
 	// Charge un fichier XML dans l'objectif courant
 	void load(string path, int id){
-	
+		XmlTextReader myXmlTextReader;
 	}
 }

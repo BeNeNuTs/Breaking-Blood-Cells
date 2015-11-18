a43eaaadbef7888393d5e451a832d254c262a654
cedrictardella <cedric.tardella@wanadoo.fr>
2015-11-18 11:02:39 +0100

    Ajout du HUD de départ

---
+++ Control/MenuController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour{
+	public void quit(){
+		if (Application.isEditor)
+			Debug.Break ();
+		else
+			Application.Quit ();
+	}
+
+	public void loadPlayLevel(string sceneName){
+		Application.LoadLevel (sceneName);
+	}
+
+	public void toReception(){
+		GameObject.Find ("ReceptionMenu").SetActive (true);
+		GameObject.Find ("PlayMenu").SetActive (false);
+		GameObject.Find ("ControlMenu").SetActive (false);
+	}
+
+	public void toControls(){
+		GameObject.Find ("ReceptionMenu").SetActive (false);
+		GameObject.Find ("PlayMenu").SetActive (false);
+		GameObject.Find ("ControlMenu").SetActive (true);
+	}
+
+	public void toPlay(){
+		GameObject.Find ("ReceptionMenu").SetActive (false);
+		GameObject.Find ("PlayMenu").SetActive (true);
+		GameObject.Find ("ControlMenu").SetActive (false);
+	}
+}
